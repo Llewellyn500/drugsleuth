29f71022edac13c731e0298acbc26dcfad01f919
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-07-16 18:16:00 +0000

    ready to start the codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,14 @@ import Head from "next/head";
 import "./globals.css";
 
 const poppins = Poppins({
-  subsets: [ "latin" ],
-  weight: "100"
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
   title: "DrugSleuth",
-  description: "DrugSleuth helps identify drugs prescribed to patients by asking a series of targeted questions tailored for both healthcare professionals and patients. Answer questions about patient details, symptoms, drug appearance, usage, and more to get accurate drug identification.",
+  description:
+    "DrugSleuth helps identify drugs prescribed to patients by asking a series of targeted questions tailored for both healthcare professionals and patients. Answer questions about patient details, symptoms, drug appearance, usage, and more to get accurate drug identification.",
 };
 
 export default function RootLayout({
@@ -23,9 +24,23 @@ export default function RootLayout({
       <Head>
         <title>{String(metadata.title)}</title>
         <meta name="description" content={metadata.description ?? ""} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#cff2fb" />
