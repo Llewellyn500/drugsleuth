dc67819e8adcf64cf2f79fa693b76aa17cb2035b
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-07-16 21:43:20 +0000

    chore: Add caching headers for static assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // Matching all static assets with these extensions
+          source: '/:all*(svg|jpg|png|webp)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable', // 1 year cache, immutable
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
